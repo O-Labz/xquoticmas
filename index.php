56868d7e52956413abf65950db6b18a4a284d53a
Omri Hopson <omri.hopson@gmail.com>
2019-01-15 08:00:53 -0500

    organised fav icons

--- index.php
+++ index.php
@@ -8,22 +8,22 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="57x57" href="./assets/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="./assets/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="./assets/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="./assets/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="./assets/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="./assets/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="./assets/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="./assets/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="./assets/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="./assets/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="./assets/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon-16x16.png">
-	<link rel="manifest" href="./assets/manifest.json">
+    <link rel="apple-touch-icon" sizes="57x57" href="./assets/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="./assets/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="./assets/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="./assets/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="./assets/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="./assets/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="./assets/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="./assets/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="./assets/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="./assets/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="./assets/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="./assets/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="./assets/favicon/favicon-16x16.png">
+	<link rel="manifest" href="./assets/favicon/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="./assets/ms-icon-144x144.png">
+	<meta name="msapplication-TileImage" content="./assets/favicon/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
 
 
@@ -44,7 +44,7 @@
 
     <img src="./assets/Mas.png">
 
-    <h1>Are you of legal age to party with Xquotic ? </h1>
+<!--     <h1>Are you of legal age to party with Xquotic ? </h1>
 
     <form action="/action_page.php">
       Birthday:
@@ -54,7 +54,7 @@
 
     
 
-    <h1>Be Exquisite; Live Exotic…</h1>
+    <h1>Be Exquisite; Live Exotic…</h1> -->
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
