740067696c599aedcd444d68e2f096f6690d090a
Omri Hopson <omri.hopson@gmail.com>
2019-01-18 18:36:29 -0500

    updated Home page

--- src/pages/index.php
+++ src/pages/index.php
@@ -8,32 +8,36 @@
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
     <!-- Favicon -->
-    <link rel="apple-touch-icon" sizes="57x57" href="../assets/favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="../assets/favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="../assets/favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="../assets/favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="../assets/favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="../assets/favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="../assets/favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="../assets/favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="../assets/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="../assets/favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="../assets/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="../assets/favicon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="../assets/favicon/favicon-16x16.png">
-	<link rel="manifest" href="../assets/favicon/manifest.json">
+    <link rel="apple-touch-icon" sizes="57x57" href="/xauticmas/assets/favicon/apple-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/xauticmas/assets/favicon/apple-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/xauticmas/assets/favicon/apple-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/xauticmas/assets/favicon/apple-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/xauticmas/assets/favicon/apple-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/xauticmas/assets/favicon/apple-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/xauticmas/assets/favicon/apple-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/xauticmas/assets/favicon/apple-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/xauticmas/assets/favicon/apple-icon-180x180.png">
+	<link rel="icon" type="image/png" sizes="192x192"  href="/xauticmas/assets/favicon/android-icon-192x192.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/xauticmas/assets/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="96x96" href="/xauticmas/assets/favicon/favicon-96x96.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/xauticmas/assets/favicon/favicon-16x16.png">
+	<link rel="manifest" href="/xauticmas/assets/favicon/manifest.json">
 	<meta name="msapplication-TileColor" content="#ffffff">
-	<meta name="msapplication-TileImage" content="../assets/favicon/ms-icon-144x144.png">
+	<meta name="msapplication-TileImage" content="/xauticmas/assets/favicon/ms-icon-144x144.png">
 	<meta name="theme-color" content="#ffffff">
 
 
     <!-- Bootstrap CSS -->
-    <link href="../stylesheet/css/bootstrap-grid.css" rel="stylesheet">
-    <link href="../stylesheet/css/bootstrap-grid.min.css" rel="stylesheet">
-    <link href="../stylesheet/css/bootstrap-reboot.css" rel="stylesheet">
-    <link href="../stylesheet/css/bootstrap-reboot.min.css" rel="stylesheet">
-    <link href="../stylesheet/css/bootstrap.css" rel="stylesheet">
-    <link href="../stylesheet/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/xauticmas/stylesheet/css/bootstrap-grid.css" rel="stylesheet">
+    <link href="/xauticmas/stylesheet/css/bootstrap-grid.min.css" rel="stylesheet">
+    <link href="/xauticmas/stylesheet/css/bootstrap-reboot.css" rel="stylesheet">
+    <link href="/xauticmas/stylesheet/css/bootstrap-reboot.min.css" rel="stylesheet">
+    <link href="/xauticmas/stylesheet/css/bootstrap.css" rel="stylesheet">
+    <link href="/xauticmas/stylesheet/css/bootstrap.min.css" rel="stylesheet">
+
+    <img src="/xauticmas/assets/MasLogo.png" id="exotic" alt="exotic" style="width: 20%; position: fixed;">
+
+
 
   </head>
 
@@ -42,11 +46,35 @@
 	<br>
 	<!-- Logo Image -->
 
-    <div class="container">
-  		<img src="../assets/MasLogo.png" id="exotic" alt="exotic">
-	</div>
+	<nav class="navbar navbar-expand-lg navbar-light" style="width: 60%; position: fixed; margin: auto;  padding-left: 2em; margin-top: 5%; right: 0; left: 0; background-color: #ffffff;">
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+	  <div class="collapse navbar-collapse" id="navbarText">
+	    <ul class="navbar-nav mr-auto">
+		  <li class="nav-item">
+		    <a class="nav-link active" href="#">Home</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">About Us</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Gallery</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Packages</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Registration</a>
+		  </li>
+		  <li class="nav-item">
+		    <a class="nav-link" href="#">Contact Us</a>
+		  </li>
+	    </ul>
+	  </div>
+	</nav>
 
-	<h1>hi</h1>
+  	
 
 
 
