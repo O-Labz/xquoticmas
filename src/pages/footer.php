34bac4ebb949831865337ced92a992f135552929
Omri Hopson <omri.hopson@gmail.com>
2019-01-28 20:51:59 -0500

    updated footer

--- src/pages/footer.php
+++ src/pages/footer.php
@@ -1,25 +1,27 @@
+
   </body>
-  <br>
-  <br>
-  <br>
-  <br>
-  <footer>
-  	<div class="container" style="margin: auto; padding-left: 14%; text-align: center; right: 0; left: 0;">
-  	   <div class="row" style="text-align: center;">
-	      <a href="#">
-		  	<img src="/xauticmas/assets/social/fb.png" alt="facebook" style="width:42px;height:42px;border:0; margin: 5px 10px;">
-		  </a>
-	      <a href="#">
-		  	<img src="/xauticmas/assets/social/insta.png" alt="instagram" style="width:42px;height:42px;border:0; margin: 5px 10px;">
-		  </a>
-		  <strong>Be Exquisite; Live Exotic…</strong>
-		  <a href="#">
-		  	<img src="/xauticmas/assets/social/twitter.png" alt="twitter" style="width:42px;height:42px;border:0; margin: 5px 10px;">
-		  </a>
-	      <a href="#">
-		  	<img src="/xauticmas/assets/social/snap.png" alt="snapchat" style="width:42px;height:42px;border:0; margin: 5px 10px;">
-		  </a>
-	    </div>
+	<div class="container-fluid fixed-bottom" style="background-color: #efefef; width: 100%; position: fixed; opacity: .9;">
+		<div class="row justify-content-md-center">
+			    <div class="col-sm-4">
+				</div>
+				<div class="col-sm-4">
+					<a href="#">
+					  	<img src="/xauticmas/assets/social/fb.png" alt="facebook" style="width:30px;height:30px;border:0; margin: 2px 5px; position:relative;">
+					  </a>
+				      <a href="#">
+					  	<img src="/xauticmas/assets/social/insta.png" alt="instagram" style="width:30px;height:30px;border:0; margin: 2px 5px; position:relative;">
+					  </a>
+				    <strong>Be Exquisite; Live Exotic…</strong>
+				    <a href="#">
+				  	<img src="/xauticmas/assets/social/twitter.png" alt="twitter" style="width:30px;height:30px;border:0; margin: 2px 5px; position:relative;">
+				  </a>
+			      <a href="#">
+				  	<img src="/xauticmas/assets/social/snap.png" alt="snapchat" style="width:30px;height:30px;border:0; margin: 2px 5px; position:relative;">
+				  </a>
+				</div>
+			    <div class="col-sm-4">
+		    	</div>
+			</div>
+		</div>
 	</div>
-  </footer>
 </html>
