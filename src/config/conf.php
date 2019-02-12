e8652f8e1dddcc9cdec65b1f534f05b944f4fa46
Omri Hopson <omri.hopson@gmail.com>
2019-02-11 23:43:36 -0500

    added production branch

--- src/config/conf.php
+++ src/config/conf.php
@@ -12,22 +12,22 @@
 	DEFINE('DB_ROOTUSER', 'root');
 	DEFINE('DB_ROOTPASSWORD', 'turtledove@1##');
 
-	DEFINE('CONTACT_LINK', 'Location: http://localhost:1234/Xquoticmas/src/pages/index.php?page=contactdone');
-	DEFINE('REGISTER_LINK', 'Location: http://localhost:1234/Xquoticmas/src/pages/index.php?page=registrationdone');
-	DEFINE('ADMIN_LINK', 'Location: http://localhost:1234/Xquoticmas/index.php');
+	DEFINE('CONTACT_LINK', 'Location: http://www.xquoticmas.com/Xquoticmas/src/pages/index.php?page=contactdone');
+	DEFINE('REGISTER_LINK', 'Location: http://www.xquoticmas.com/Xquoticmas/src/pages/index.php?page=registrationdone');
+	DEFINE('ADMIN_LINK', 'Location: http://www.xquoticmas.com/Xquoticmas/index.php');
 
-	DEFINE('WELCOME_LINK','http://localhost:1234/Xquoticmas/customorder.php');
-	DEFINE('REG_LINK','http://localhost:1234/Xquoticmas/registered.php');
-	DEFINE('COMMENTS_LINK','http://localhost:1234/Xquoticmas/Comments.php');
-	DEFINE('DRINKS_LINK','http://localhost:1234/Xquoticmas/drinks.php');
-	DEFINE('TSHIRT_LINK','http://localhost:1234/Xquoticmas/tshirtndrinks.php');
-	DEFINE('COSTUME_LINK','http://localhost:1234/Xquoticmas/costumendrinks.php');
-	DEFINE('BASICD_LINK','http://localhost:1234/Xquoticmas/basicshirt.php');
-	DEFINE('BASICC_LINK','http://localhost:1234/Xquoticmas/basiccostume.php');
-	DEFINE('PREMIUMS_LINK','http://localhost:1234/Xquoticmas/premiumshirt.php');
-	DEFINE('COSTUMED_LINK','http://localhost:1234/Xquoticmas/costumendrinks.php');
-	DEFINE('PREMIUMC_LINK','http://localhost:1234/Xquoticmas/premiumcostume.php');
-	DEFINE('CUSTOMO_LINK','http://localhost:1234/Xquoticmas/customeorders.php');
+	DEFINE('WELCOME_LINK','http://www.xquoticmas.com/Xquoticmas/customorder.php');
+	DEFINE('REG_LINK','http://www.xquoticmas.com/Xquoticmas/registered.php');
+	DEFINE('COMMENTS_LINK','http://www.xquoticmas.com/Xquoticmas/Comments.php');
+	DEFINE('DRINKS_LINK','http://www.xquoticmas.com/Xquoticmas/drinks.php');
+	DEFINE('TSHIRT_LINK','http://www.xquoticmas.com/Xquoticmas/tshirtndrinks.php');
+	DEFINE('COSTUME_LINK','http://www.xquoticmas.com/Xquoticmas/costumendrinks.php');
+	DEFINE('BASICD_LINK','http://www.xquoticmas.com/Xquoticmas/basicshirt.php');
+	DEFINE('BASICC_LINK','http://www.xquoticmas.com/Xquoticmas/basiccostume.php');
+	DEFINE('PREMIUMS_LINK','http://www.xquoticmas.com/Xquoticmas/premiumshirt.php');
+	DEFINE('COSTUMED_LINK','http://www.xquoticmas.com/Xquoticmas/costumendrinks.php');
+	DEFINE('PREMIUMC_LINK','http://www.xquoticmas.com/Xquoticmas/premiumcostume.php');
+	DEFINE('CUSTOMO_LINK','http://www.xquoticmas.com/Xquoticmas/customeorders.php');
 
 	$abc = @mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME) or die("Could not connect to MYSQL: ".mysqli_connect_error());
 
